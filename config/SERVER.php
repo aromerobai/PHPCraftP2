ab6d08fc1a6487517050b209d7969ad37d3d6c95
aromerobai <aromerobai@uoc.edu>
2023-11-06 12:57:40 +0100

    "Archivos de configuracion"

---
+++ config/SERVER.php
@@ -0,0 +1,11 @@
+<?php
+    const SERVER = "localhost";
+    const DB = "eventos";
+    const USER = "root";
+    const PASS = "test";
+
+    const SGBD = "mysql:host".SERVER.";dbname=".DB;
+
+    const METHOD = "AES-256-CBC";
+    const SECRET_KEY = '$EVENTOS@P2';
+    const SECRET_IV = "123456";
